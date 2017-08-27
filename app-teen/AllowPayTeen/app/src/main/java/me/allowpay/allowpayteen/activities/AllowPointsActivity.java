0246405518d24dd2aaeb7659f99d9212831fa4d4
[TGL] Dogg <vinidoggll@hotmail.com>
2017-08-27 02:56:59 -0300

    Tá muito top isso, mano

---
+++ app-teen/AllowPayTeen/app/src/main/java/me/allowpay/allowpayteen/activities/AllowPointsActivity.java
@@ -0,0 +1,15 @@
+package me.allowpay.allowpayteen.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import me.allowpay.allowpayteen.R;
+
+public class AllowPointsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_allow_points);
+    }
+}
